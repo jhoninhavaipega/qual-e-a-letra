1df98b0b9b77f4c11a4f16441cffa7f6a4d0c666
Jhonatan Silva <admin@jhonatansilva.com>
2015-02-28 01:17:59 -0300

    adicionado metodo search track

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,10 +1,28 @@
+var request = require('request'),
+    urlencode = require('urlencode');
+
 module.exports = function( app ) {
 
   var controller = {};
 
   controller.all = function( req, res ) {
 
-    console.info(req.query.q);
+    var key = 'c319408908cd3fa96fb77623d0b79340',
+        urlApi = 'http://api.musixmatch.com/ws/1.1/',
+        urlSearchTrack = urlApi + 'track.search?apikey=' + key + '&q_track=' + urlencode(req.query.q),
+        urlSearchArtist = urlApi + 'artist.search?apikey=' + key + '&q_artist=' + urlencode(req.query.q);
+
+    request(urlSearchTrack, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.json(JSON.parse(body));
+      }
+    });
+
+    // request(urlSearchArtist, function (error, response, body) {
+    //   if (!error && response.statusCode == 200) {
+    //     res.json(JSON.parse(body));
+    //   }
+    // });
 
   };
 
