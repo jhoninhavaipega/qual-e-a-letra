40e09320c93ef9f2562346e6741204244c6487cd
Jhonatan Silva <admin@jhonatansilva.com>
2015-03-03 01:14:51 -0300

    adicionado forEach na busca por artista e musica

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -3,26 +3,35 @@ var request = require('request'),
 
 module.exports = function( app ) {
 
-  var controller = {};
+  var controller = {},
+      key = 'c319408908cd3fa96fb77623d0b79340',
+      urlApi = 'http://api.musixmatch.com/ws/1.1/';
 
   controller.all = function( req, res ) {
 
-    var key = 'c319408908cd3fa96fb77623d0b79340',
-        urlApi = 'http://api.musixmatch.com/ws/1.1/',
-        urlSearchTrack = urlApi + 'track.search?apikey=' + key + '&q_track=' + urlencode(req.query.q),
-        urlSearchArtist = urlApi + 'artist.search?apikey=' + key + '&q_artist=' + urlencode(req.query.q);
+    var i = 0,
+        arrResponse = [],
+        urls = [
+                  urlApi + 'track.search?apikey=' + key + '&q_track=' + urlencode(req.query.q),
+                  urlApi + 'artist.search?apikey=' + key + '&q_artist=' + urlencode(req.query.q)
+                ];
 
-    request(urlSearchTrack, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        res.json(JSON.parse(body));
-      }
+    urls.forEach(function( url, index ) {
+
+      request(url, function (error, response, body) {
+        i++;
+
+        if (!error && response.statusCode == 200) {
+          arrResponse.push(JSON.parse(body));
+        }
+
+        if ( i == urls.length ) {
+          console.log('foi');
+          res.json(arrResponse);
+        }
+      });
+
     });
-
-    // request(urlSearchArtist, function (error, response, body) {
-    //   if (!error && response.statusCode == 200) {
-    //     res.json(JSON.parse(body));
-    //   }
-    // });
 
   };
 
